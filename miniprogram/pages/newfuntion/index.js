db7fa04f7787faf8ac5f0b48d7b843002e71d49c
yyx <273839986@qq.com>
2024-01-08 21:00:01 +0800

    新的添加功能1.8

---
+++ miniprogram/pages/newfuntion/index.js
@@ -0,0 +1,81 @@
+// pages/newfuntion/index.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    databaseData: [],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  
+  onLoad: function () {
+    this.getDatabaseData();
+  },
+  getDatabaseData: function () {
+    const db = wx.cloud.database();
+
+    // 查询数据库中的数据
+    db.collection('images').get().then(res => {
+      console.log(res);
+      this.setData({
+        databaseData: res.data,
+      });
+      console.log(this.data.databaseData)
+    }).catch(error => {
+      console.error('Error fetching data from database:', error);
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
